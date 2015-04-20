ab0a3c582651411db1f4a50da742936d47bdadc8
Jeff Wofford <github@jeffwofford.com>
2015-04-20 10:20:53 -0400

    Emscripten: Forcing inclusion of ParticleEmitter.

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,26 @@
 
 #include "ApplicationStaged.h"
 #include "FreshFile.h"
+#include "ParticleEmitter.h"
 using namespace fr;
+
+namespace
+{
+	template< typename T >
+	void use()
+	{
+		typename T::ptr t = createObject< T >();
+	}
+}
+
+namespace ld
+{
+	void forceInclusion()
+	{
+		// Reference Fresh static library functions and classes that might otherwise be stripped due to apparent non-use.
+		use< fr::ParticleEmitter >();
+	}
+}
 
 int main( int argc, char* argv[] )
 {
